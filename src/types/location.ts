e7a977716f9528354c3639bc129b94e8881667fa
Ardi Germenji <67866525+a-pyro@users.noreply.github.com>
2021-05-28 14:18:53 +0200

    adds types interfaces

---
+++ src/types/location.ts
@@ -0,0 +1,13 @@
+export interface City {
+  formattedAddress: string;
+  latitude: number;
+  longitude: number;
+  country: string;
+  city: string;
+  stateCode: string;
+  zipcode: string;
+  streetName: string;
+  streetNumber: null;
+  countryCode: string;
+  provider: string;
+}
